00febfcf957b7335a482979698f9055619f8c79c
John Lapeyre <john.lapeyre@gmail.com>
2014-12-11 19:45:25 +0100

    start doc == for matrices 	modified:   README.md 	modified:   src/permmat.jl

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # PermutationsA
 
 [![Build Status](https://travis-ci.org/jlapeyre/PermutationsA.jl.svg?branch=master)](https://travis-ci.org/jlapeyre/PermutationsA.jl)
+
+Data types and methods for permutations
+
+### Overview
+
+This module implements three representations of permutations: list,
+disjoint cycles, and matrix. The name is `PermuationsA' to distinguish it
+from the module `Permutations'. This module wraps generic routines in
+the module `PermPlain'.
+
--- src/permmat.jl
+++ src/permmat.jl
@@ -3,7 +3,7 @@ export PermMat
 export randpermmat, idpermmat
 #, order, sign, commute, idpermmat
 
-import Base: full, getindex, size, similar, copy, eltype, ctranspose,
+import Base: full, sparse, getindex, size, similar, copy, eltype, ctranspose,
              transpose, one
 
 # Several methods are defined in permallrep.jl, which is loaded after
@@ -21,23 +21,33 @@ idpermmat(n::Integer) = PermMat([1:n])
 idpermmat(T::DataType, n::Integer) = PermMat([one(T):convert(T,n)])
 idperm{T}(m::PermMat{T}) = PermMat([one(T):convert(T,length(m.data))])
 one(m::PermMat) = idperm(m)
+plength(m::PermMat) = length(m.data)
 
 ## size, copy, indexing ##
 
 getindex{T}(m::PermMat{T}, i::Integer, j::Integer) =  m.data[j] == i ? one(T) : zero(T)
 
 function getindex{T}(m::PermMat{T}, k::Integer)
-    i,j = divrem(k-1,length(m.data))
+    i,j = divrem(k-1,plength(m))
     return m.data[j+1] == i+1 ? one(T) : zero(T)
 end     
 
-size{T}(m::PermMat{T}) = (s = length(m.data); (s,s))
+size{T}(m::PermMat{T}) = (s = plength(m); (s,s))
 copy(m::PermMat) = PermMat(copy(m.data))
 similar(m::PermMat, atype, dims) = Array(atype, dims)
 eltype{T}(m::PermMat{T}) = T
 
 # dense matrix
 full(m::PermMat) = [m[i,j] for i=1:size(m,1), j=1:size(m,2)]
+
+# sparse matrix
+function sparse{T}(m::PermMat{T})
+    n = plength(m)
+    I = [1:n]
+    J = (m.data)[I]
+    V = ones(T,n)
+    sparse(J,I,V)
+end
 
 ## operators ##
 
@@ -49,13 +59,44 @@ inv(m::PermMat) = PermMat(invperm(m.data))
 *(m::PermMat, v::String) = v[m.data]
 *(m1::PermMat, m2::PermMat) = PermMat(PermPlain.permcompose(m2.data,m1.data))
 /(m1::PermMat, m2::PermMat) = PermMat(PermPlain.permcompose(m2.data,invperm(m1.data)))
-*(m::PermMat, k::Integer) = k > length(m.data) ? k : m.data[k]
+*(m::PermMat, k::Integer) = k > plength(m) ? k : m.data[k]
 /(k::Integer, m::PermMat) = PermPlain.preimage(m.data,k)
 \(m::PermMat, k::Int) = k / m
 #*(m::PermMat, v::Vector) = v[m.data]
 *{T}(m::PermMat, a::AbstractVector{T}) = [ m * i for i in a]
 ctranspose(m::PermMat) = inv(m)
 transpose(m::PermMat) = inv(m)
+
+function ==(pm::PermMat, m::AbstractMatrix)
+    (n1,n2) = size(m)
+    d = pm.data
+    n1 == n2 || return false
+    for j in 1:n1
+        for i in 1:n1
+            val = m[i,j]
+            if val != 0
+                ((val == 1 && d[j] == i) || return false)
+            end
+        end
+    end
+    return true
+end
+
+# could factor this code somehow
+function ==(m::AbstractMatrix, pm::PermMat)
+    (n1,n2) = size(m)
+    d = pm.data
+    n1 == n2 || return false
+    for j in 1:n1
+        for i in 1:n1
+            val = m[i,j]
+            if val != 0
+                ((val == 1 && d[j] == i) || return false)
+            end
+        end
+    end
+    return true
+end
 
 # Wikipedia says that if M_i represents p_i, then
 # M_1 * M_2  <---> p_2 ∘ p_1
