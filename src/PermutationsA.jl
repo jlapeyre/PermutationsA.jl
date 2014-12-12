6a4bb2269f6c2e3997352c2d207b780a7dbd756d
John Lapeyre <john.lapeyre@gmail.com>
2014-12-13 00:59:16 +0100

    check, import/export fixes

--- src/PermutationsA.jl
+++ src/PermutationsA.jl
@@ -1,25 +1,15 @@
 module PermutationsA
 
-include("abstractperm.jl")
-
-export getindex,setindex!,cycles,cyclelengths,sign,order,
-       cycletype,div2,trans,randpermlist,permlist,matrix,
-       permcycs, copy, idpermlist, isid, inv,
-       distance, same, convert,ppow, list
+export permlist, order, idpermlist, matrix, distance, permcycs
+export cycles, cycletype, cyclelengths
 
 using PermPlain
-
-export randpermcycs
-
 import Base: length, sign, copy, convert, sparse, full
 
+include("abstractperm.jl")
 include("permlist.jl")
 include("permcycs.jl")
 include("permmat.jl")
 include("permsparse.jl")
 include("permallrep.jl")
 end # module PermutationsA
-
-# TODO:
-# see pari/gp as well
-# implement iterators, for mapping, etc.
